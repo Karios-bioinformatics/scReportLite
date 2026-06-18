2e0677216ae074dc4483eeecb906af8ebe90897d
Karios <karioslion0517@gmail.com>
2026-06-18 21:09:07 +0800

    Update sc_report.R

--- R/sc_report.R
+++ R/sc_report.R
@@ -1077,7 +1077,7 @@ function renderPcLoading() {
   area.style.display = "";
 
   if (!_PCA_LOADING || _PCA_LOADING.length === 0) {
-    content.innerHTML = "<p class=\"no-data\">No PCA loading data provided.</p>";
+    content.innerHTML = "<p class=\\"no-data\\">No PCA loading data provided.</p>";
     return;
   }
 
@@ -1091,7 +1091,7 @@ function renderPcLoading() {
   }
 
   if (rows.length === 0) {
-    content.innerHTML = "<p class=\"no-data\">No loading data for " + pc + ".</p>";
+    content.innerHTML = "<p class=\\"no-data\\">No loading data for " + pc + ".</p>";
     return;
   }
 
